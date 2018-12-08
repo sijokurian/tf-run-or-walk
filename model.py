dae35a3b9534c0823d0c6a2fe17850bd8daf3b3b
Sijo <sijo_tk@ispg.in>
2018-12-08 11:48:28 +0530

    Variable

--- model.py
+++ model.py
@@ -1,12 +1,17 @@
 import tensorflow as tf
 
-node1 = tf.constant(5,tf.int8)
-node2 = tf.constant(2,tf.int8)
+W = tf.Variable([.3],tf.float32)
+b = tf.Variable([.3],tf.float32)
+x = tf.placeholder(tf.float32)
 
-node3 = node2 + node1
+
+linear_model = W * x + b
 sess = tf.Session()
+var_init = tf.global_variables_initializer()
+sess.run(var_init)
 
-file_writer = tf.summary.FileWriter('C:\\Users\\sijo.ISPG\\Documents\\work\\TF\\Study\\run-or-walk\\graph',sess.graph)
-print(sess.run(node3))
+file_writer = tf.summary.FileWriter('C:\\Users\\sijo.ISPG\\Documents\\work\\TF\\Study\\tf-run-or-walk\\tf-run-or-walk\\graph',sess.graph)
+
+print(sess.run(linear_model,{x:[1,2]}))
 
 sess.close()
